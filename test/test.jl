d66a68e1b24602a3afa46ddf4da7658eb791f6c5
Jamie Fairbrother <jfairbrother87@hotmail.com>
2014-10-16 12:36:22 +0100

    - Made calculation of moments in test.jl cleaner

--- test/test.jl
+++ test/test.jl
@@ -3,15 +3,10 @@ using Base.Test
 
 function moments(x::Array{Float64, 2}, probs::Array{Float64, 1})
     dim = size(x,1)
-    m = Array(Float64, dim)
-    for i=1:dim m[i] = dot(scenarios[i,:], probs) end
-    m2 = Array(Float64, dim)
-    for i=1:dim m2[i] = dot(scenarios[i,:].^2, probs) end
-    m3 = Array(Float64, dim)
-    for i=1:dim m3[i] = dot(scenarios[i,:].^3, probs) end
-    m4 = Array(Float64, dim)
-    for i=1:dim m4[i] = dot(scenarios[i,:].^4, probs) end
-
+    m = scenarios * probs
+    m2 = scenarios.^2 * probs
+    m3 = scenarios.^3 * probs
+    m4 = scenarios.^4 * probs
     v = m2 - m.^2
     sd = sqrt(v)
     mom_matrix = Array(Float64, dim, 4)
@@ -45,5 +40,5 @@ num_scen = 1000
 scenarios = scengen_HKW(tg_moms, tg_cors, num_scen)
 probs = fill(1.0/num_scen, num_scen)
 
-@test norm(moments(scenarios) - tg_moms) < 1e-3
+@test norm(moments(scenarios, probs) - tg_moms) < 1e-3
 @test norm(cor(scenarios, vardim=2) - tg_cors) < 1e-3
