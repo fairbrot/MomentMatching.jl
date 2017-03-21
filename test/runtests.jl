1914dd8b7dc273131e82fce37ea4a58204fd92aa
Jamie Fairbrother <jfairbrother87@hotmail.com>
2017-03-21 15:53:12 +0000

    Fixed deprecations bugs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using MomentMatching
 using Base.Test
 
-tg_moms = [[0.0 1.0 1.0 0.0], [1.0 2.0 -1.0 1.0]]
-tg_cors = [[1 0.5], [0.5 1]]
+tg_moms = [[0.0 1.0 1.0 0.0]; [1.0 2.0 -1.0 1.0]]
+tg_cors = [[1 0.5]; [0.5 1]]
 num_scen = 1000
 scenarios = scengen_HKW(tg_moms, tg_cors, num_scen)
 probs = fill(1.0/num_scen, num_scen)
 
 moms = moments(scenarios, probs)
-cors = cor(scenarios, vardim=2)
+cors = Base.corm(scenarios, mean(scenarios, 2), 2)
 
 @test norm(moms - tg_moms) < 1e-3
 @test norm(cors - tg_cors) < 1e-3
