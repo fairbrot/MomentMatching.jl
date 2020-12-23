ffac178b59705bd0e83544a7f77fad406fc12612
bnarum <b_narum@outlook.com>
2020-12-23 18:51:39 +0100

    Update to new Pkg.jl convension. Replace REQUIRE with Project.jl and add separate test requirements.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,16 @@ using LinearAlgebra
 using Statistics
 using Test
 
-tg_moms = [[0.0 1.0 1.0 0.0]; [1.0 2.0 -1.0 1.0]]
-tg_cors = [[1 0.5]; [0.5 1]]
-num_scen = 1000
-scenarios = scengen_HKW(tg_moms, tg_cors, num_scen)
-probs = fill(1.0/num_scen, num_scen)
+@testset "Simple testset from HKW" begin
+    tg_moms = [[0.0 1.0 1.0 0.0]; [1.0 2.0 -1.0 1.0]]
+    tg_cors = [[1 0.5]; [0.5 1]]
+    num_scen = 1000
+    scenarios = scengen_HKW(tg_moms, tg_cors, num_scen)
+    probs = fill(1.0/num_scen, num_scen)
 
-moms = moments(scenarios, probs)
-cors = cor(scenarios, dims=2)
+    moms = moments(scenarios, probs)
+    cors = cor(scenarios, dims=2)
 
-@test norm(moms - tg_moms, Inf) < 1e-3
-@test norm(cors - tg_cors, Inf) < 1e-3
+    @test norm(moms - tg_moms, Inf) < 1e-3
+    @test norm(cors - tg_cors, Inf) < 1e-3
+end
