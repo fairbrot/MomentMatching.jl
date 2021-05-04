2ecd319b61fc0ea86a331beca2f62574a2b87ff2
bnarum <b_narum@outlook.com>
2021-05-04 08:07:10 +0200

    Added Windows command as alternative. Could have make on the system from before.

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,5 @@
 cd(joinpath(@__DIR__, "HoylandKautWallace"))
+run(`make`)
 
-if Sys.isunix()
-    run(`make`)
-elseif Sys.iswindows()
-    run(`mingw32-make`) # See guide for Windows at https://www.codementor.io/@evalparse/making-compiling-c-functions-for-use-in-julia-on-windows-f9lwa5i43
-end
+# See guide for Windows at https://www.codementor.io/@evalparse/making-compiling-c-functions-for-use-in-julia-on-windows-f9lwa5i43
+# Alternative: run(`mingw32-make`) 
