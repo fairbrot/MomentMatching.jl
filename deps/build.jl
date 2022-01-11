ea1f0a3b6ffc1611e3b7296b8a83b3deaadaaf2b
Benjamin Narum <b_narum@outlook.com>
2022-01-11 12:24:58 +0100

    Add Windows specific call

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,9 @@
 cd(joinpath(@__DIR__, "HoylandKautWallace"))
-run(`make`)
 
-# See guide for Windows at https://www.codementor.io/@evalparse/making-compiling-c-functions-for-use-in-julia-on-windows-f9lwa5i43
-# Alternative: run(`mingw32-make`) 
+if Sys.isunix()
+    run(`make`)
+elseif Sys.iswindows()
+    # See guide for Windows at https://www.codementor.io/@evalparse/making-compiling-c-functions-for-use-in-julia-on-windows-f9lwa5i43
+    # Download MinGW-w64: https://www.mingw-w64.org/downloads/#mingw-builds and add bin to path
+    run(`mingw32-make`)
+end
